6d140df67f76f4e0dc26c8d6134620c8a705dfe2
Çağrı Acar <cagriacar22@gmail.com>
2020-10-15 01:56:51 +0300

    react-website-v1

---
+++ src/components/pages/Footer.js/Footer.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import './Footer.css';
+import { Button } from '../../Button';
+import { Link } from 'react-router-dom';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin
+} from 'react-icons/fa';
+import { MdFingerprint } from 'react-icons/md';
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <section className='footer-subscription'>
+        <p className='footer-subscription-heading'>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, veritatis?
+        </p>
+        <p className='footer-subscription-text'>
+          Bize Ulaşın.
+        </p>
+        <div className='input-areas'>
+          <form>
+            <input
+              className='footer-input'
+              name='email'
+              type='email'
+              placeholder='Email Address'
+            />
+            <Button buttonStyle='btn--outline'>Gönder</Button>
+          </form>
+        </div>
+      </section>
+      <div className='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div className='footer-link-items'>
+            <h2>Hakkımızda</h2>
+            <Link to='/sign-up'>How it works</Link>
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>Careers</Link>
+            <Link to='/'>Investors</Link>
+            <Link to='/'>Terms of Service</Link>
+          </div>
+          <div className='footer-link-items'>
+            <h2>İletişim</h2>
+            <Link to='/'>Contact</Link>
+            <Link to='/'>Support</Link>
+            <Link to='/'>Destinations</Link>
+            <Link to='/'>Sponsorships</Link>
+          </div>
+        </div>
+        <div className='footer-link-wrapper'>
+          <div className='footer-link-items'>
+            <h2>Haberler</h2>
+            <Link to='/'>Submit Video</Link>
+            <Link to='/'>Ambassadors</Link>
+            <Link to='/'>Agency</Link>
+            <Link to='/'>Influencer</Link>
+          </div>
+          <div className='footer-link-items'>
+            <h2>Sosyal Medya</h2>
+            <Link to='/'>Instagram</Link>
+            <Link to='/'>Facebook</Link>
+            <Link to='/'>Youtube</Link>
+            <Link to='/'>Twitter</Link>
+          </div>
+        </div>
+      </div>
+      <section className='social-media'>
+        <div className='social-media-wrap'>
+          <div className='footer-logo'>
+            <Link to='/' className='social-logo'>
+              <MdFingerprint className='navbar-icon' />
+              Demo Tip
+            </Link>
+          </div>
+          <small className='website-rights'>Demo Tip © 2020</small>
+          <div className='social-icons'>
+            <Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='Facebook'
+            >
+              <FaFacebook />
+            </Link>
+            <Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='Instagram'
+            >
+              <FaInstagram />
+            </Link>
+            <Link
+              className='social-icon-link'
+              to={
+                '//www.youtube.com/channel/UCsKsymTY_4BYR-wytLjex7A?view_as=subscriber'
+              }
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <FaYoutube />
+            </Link>
+            <Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='Twitter'
+            >
+              <FaTwitter />
+            </Link>
+            <Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='LinkedIn'
+            >
+              <FaLinkedin />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
